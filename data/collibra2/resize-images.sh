59f571f68cce8c9f713264bcd1fb0d58f05968aa
Kevin Luzbetak <luzbetak5739@gmail.com>
2024-10-09 09:46:26 -0700

    2024-10-09 09:46

--- data/collibra2/resize-images.sh
+++ data/collibra2/resize-images.sh
@@ -2,8 +2,8 @@
 
 # Loop through all PNG files in the current directory
 for file in *.png; do
-  # Use ImageMagick's convert command to resize the image
-  convert "$file" -resize 400x "$file"
+  # Use ImageMagick's magick command to resize the image
+  magick "$file" -resize 400x "$file"
 done
 
 echo "All images have been resized to 400px width."
