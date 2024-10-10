6dfe02adaab283b526fa4efe639eeb56c204b4d9
Kevin Luzbetak <luzbetak5739@gmail.com>
2024-10-09 21:32:05 -0700

    2024-10-09 21:32

--- data/collibra/1-rename-files.sh
+++ data/collibra/1-rename-files.sh
@@ -1,15 +1,18 @@
-#!/bin/bash -x
+#!/bin/bash
 
 # Initialize the counter
 counter=1
 
 # Loop through all the files with spaces handled properly
-ls -1t Screenshot*.png | while IFS= read -r file; do
+ls -1tr *.png | while IFS= read -r file; do
   # Format the counter to two digits
   formatted_counter=$(printf "%02d" $counter)
-  
+ 
+  # Echo the command before executing it
+  echo mv "$file" "databricks-$formatted_counter.png"
+
   # Rename the file
-  mv "$file" "collibra-$formatted_counter.png"
+  mv "$file" "databricks-$formatted_counter.png"
   
   # Increment the counter
   counter=$((counter + 1))
