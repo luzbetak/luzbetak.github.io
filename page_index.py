85310463514f8553118e487a08a80e57c7d02ce6
Kevin Luzbetak <luzbetak5739@gmail.com>
2025-11-06 14:19:49 -0800

    2025-11-06 14:19

--- page_index.py
+++ page_index.py
@@ -25,7 +25,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 # Hard-coded excluded directory patterns (excluding output directory pattern)
-EXCLUDED_DIRS = ['*includes', 'custom*dir', 'animation', 'images', 'search']
+EXCLUDED_DIRS = ['*includes', 'custom*dir', 'animation', 'images', 'search', 'autoReply']
 
 @dataclass
 class IndexEntry:
